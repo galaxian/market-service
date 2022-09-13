ac464ee347fbaeaf6b98f447d9b561b6ebcec822
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-13 21:18:26 +0900

    feat(setting) : config 설정

---
+++ src/configs/swagger.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('market-service API docs')
+    .setDescription('market-service API description')
+    .setVersion('1.0.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+import { VersioningType } from '@nestjs/common';
+import { setupSwagger } from './configs/swagger';
+
+const serverConfig = config.get('server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+    defaultVersion: '1',
+  });
+
+  app.setGlobalPrefix('/api');
+
+  setupSwagger(app);
+  const port = serverConfig.port;
+
+  await app.listen(port);
 }
 bootstrap();
