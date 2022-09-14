752a350b260606f7b6eddbb131fbb4fda3725add
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-14 17:57:31 +0900

    fix : app 및 main.ts 위치 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from 'src/configs/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { ProductModule } from './product/product.module';
+import { ProductModule } from './apps/product/product.module';
+import { UserModule } from './apps/user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule, ProductModule],
