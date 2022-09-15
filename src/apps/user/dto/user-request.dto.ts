975a34fca38b2bfb0b8a8e1b7a197d09ed0ecdbe
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-15 19:46:54 +0900

    feat : dto 유효성 검사 추가

--- src/apps/user/dto/user-request.dto.ts
+++ src/apps/user/dto/user-request.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
 
 export class UserReqeustDto {
   @ApiProperty({
@@ -8,6 +8,10 @@ export class UserReqeustDto {
     description: '사용자 이메일',
   })
   @IsNotEmpty()
+  @IsString()
+  @Matches(
+    '/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i',
+  )
   readonly email: string;
 
   @ApiProperty({
@@ -16,6 +20,8 @@ export class UserReqeustDto {
     description: '비밀번호',
   })
   @IsNotEmpty()
+  @IsString()
+  @Matches('/^.*(?=^.{10,15}$)(?=.*d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/')
   readonly password: string;
 
   @ApiProperty({
@@ -24,5 +30,7 @@ export class UserReqeustDto {
     description: '사용자 아이디',
   })
   @IsNotEmpty()
+  @IsString()
+  @MaxLength(10)
   readonly username: string;
 }
