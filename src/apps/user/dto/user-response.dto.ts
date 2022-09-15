2aac546c186c7e343af7d109e54e216f51c36970
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-15 09:21:42 +0900

    docs(user) : user api swagger 정리

--- src/apps/user/dto/user-response.dto.ts
+++ src/apps/user/dto/user-response.dto.ts
@@ -1,14 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
+import { Authority } from '../entity/user.authority';
 
 export class UserResponseDto {
+  @ApiProperty({ type: Number, example: 1, description: '사용자 pk' })
+  @IsNotEmpty()
   id: number;
 
+  @ApiProperty({
+    type: String,
+    example: 'abcd1234@gmail.com',
+    description: '사용자 이메일',
+  })
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    type: String,
+    example: '아무개',
+    description: '사용자 아이디',
+  })
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    type: Authority,
+    example: 'USER',
+    description: '사용자 권한',
+  })
   @IsNotEmpty()
-  authority: string;
+  authority: Authority;
 }
--- src/apps/user/user.controller.ts
+++ src/apps/user/user.controller.ts
@@ -9,22 +9,32 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 import { UserReqeustDto } from './dto/user-request.dto';
 import { UserResponseDto } from './dto/user-response.dto';
 import { AuthGuard } from './security/auth.guard';
 import { UserService } from './user.service';
 
+@ApiTags('회원가입 및 로그인 Api')
 @Controller({ path: '/users', version: ['1', '2'] })
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({
+    summary: '회원가입 api',
+    description: '이메일, 아이디, 비밀번호를 입력받아 회원가입을 진행한다.',
+  })
   @Post('/signup')
   @UsePipes(ValidationPipe)
   signUpUser(@Body() userRequestDto: UserReqeustDto): Promise<UserResponseDto> {
     return this.userService.signUpUser(userRequestDto);
   }
 
+  @ApiOperation({
+    summary: '로그인 api',
+    description: '아이디와 비밀번호로 본인인증 후 access token 발급',
+  })
   @Post('/signin')
   @UsePipes(ValidationPipe)
   async signInUser(
@@ -36,6 +46,10 @@ export class UserController {
     return res.json(jwt);
   }
 
+  @ApiOperation({
+    summary: '토큰 검사 api',
+    description: 'access token을 테스트 하는 api',
+  })
   @Get('/authentipicate')
   @UseGuards(AuthGuard)
   isAuthenticated(@Req() req: Request): any {
