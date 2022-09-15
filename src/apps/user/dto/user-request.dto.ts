b02c737793aeb467f09fdfae3a222f383824fa52
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-15 10:28:03 +0900

    chore(user) : 사용하지 않는 코드 제거

--- src/apps/user/dto/user-request.dto.ts
+++ src/apps/user/dto/user-request.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
-import { User } from '../entity/user.entity';
 
 export class UserReqeustDto {
   @ApiProperty({
